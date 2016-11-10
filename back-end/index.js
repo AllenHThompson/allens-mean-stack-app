e2057a0e3a94369e287ba6c9c6ebd9346843dc11
Allen H. Thompson <allenhthompson1@gmail.com>
2016-11-10 16:11:36 -0500

    Added nam CORS module for cross domain requests.

--- back-end/index.js
+++ back-end/index.js
@@ -1,11 +1,16 @@
 /* REQUIRE EXPRESS MODULE*/
 var express = require('express');
 var app = express();
+
 /* ADDED BODY PARSER B/C NOT ALLOWED ACCES*/
 var bodyParser = require('body-parser');
+/* ADDED CORS TO DO A CROSS DOMAIN REQUEST*/
+var cors = require('cors');
+
+app.use(cors());
 
 /* GLOBAL VARIABLES*/
-var colorOptions = ['red', 'yellow', 'green', 'blue', 'orange'];
+var colorOptions = ['red', 'yellow', 'green', 'blue', 'orange', 'purple'];
 
 /* ROUTES */
 app.get('/options', function(request, response) {
