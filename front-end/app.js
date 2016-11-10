05046cf4b7ca4e85d618665d54946ce2fb6c817a
Allen H. Thompson <allenhthompson1@gmail.com>
2016-11-10 16:11:07 -0500

    Removed console.log for testing.

--- front-end/app.js
+++ front-end/app.js
@@ -11,7 +11,6 @@ app.config(function($routeProvider) {
 var API = 'http://localhost:8000';
 
 app.controller('mainController', function($scope, $http, $location) {
-     console.log("IN HTTP FUNCTION");
      $http.get(API + '/options')
      .success(function(options) {
           $scope.colorOptions = options;
